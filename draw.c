457e2c0526631d6bc617b8a72e10ab1762bbdf6b
BobtheKebab <ahbab.ashraf@gmail.com>
2018-02-11 20:15:34 -0500

    Done

--- draw.c
+++ draw.c
@@ -7,4 +7,63 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+
+  int x = x0, y = y0, A = y1 - y0, B = x1 - x0, d = 2 * A - B;
+
+  if (x0 > x1) {
+    draw_line(x1, y1, x0, y0, s, c);
+  }
+  
+  //Oct 1
+  if (A >= 0 && B >= A) {
+    while (x <= x1) {
+      plot(s, c, x, y);
+      if (d > 0) {
+	y++;
+	d += 2 * B;
+      }
+      x++;
+      d += 2 * A;
+    }
+  }
+
+  //Oct 2
+  if (A >= 0 && A >= B) {
+    while (y <= y1) {
+      plot(s, c, x, y);
+      if (d < 0) {
+	x++;
+	d += 2 * A;
+      }
+      y++;
+      d += 2 * B;
+    }
+  }
+
+  //Oct 7
+  if (A < 0 && A >= (-1 *B)) {
+    while (x <= x1) {
+      plot(s, c, x, y);
+      if (d > 0) {
+	y--;
+	d += 2 * B;
+      }
+      x++;
+      d -= 2 * A;
+    }
+  }
+
+  //Oct 8
+  if (A < 0 && (-1 * B) >= A) {
+    while (y >= y1) {
+      plot(s, c, x, y);
+      if (d < 0) {
+	x++;
+	d -= 2 * A;
+      }
+      y--;
+      d += 2 * B;
+    }
+  }
+  
 }
--- main.c
+++ main.c
@@ -17,6 +17,18 @@ int main() {
 
   clear_screen(s);
 
+  draw_line(0, 0, 150, 150, s, c);
+  draw_line(250, 0, 250, 150, s, c);
+  draw_line(0, 500, 150, 350, s, c);
+  draw_line(0, 250, 150, 250, s, c);
+  draw_line(250, 500, 250, 350, s, c);
+  draw_line(500, 500, 350, 350, s, c);
+  draw_line(500, 250, 350, 250, s, c);
+  draw_line(500, 0, 350, 150, s, c);
+  draw_line(150, 150, 150, 350, s, c);
+  draw_line(150, 150, 350, 150, s, c);
+  draw_line(350, 350, 150, 350, s, c);
+  draw_line(350, 350, 350, 150, s, c);
 
   display(s);
   save_extension(s, "lines.png");
